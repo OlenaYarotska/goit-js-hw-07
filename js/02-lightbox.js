aa218814adde0453c1b23ead772fd26a3c7ce8c1
OlenaYarotska <olena.zhurba@gmail.com>
2021-10-21 18:09:21 +0300

    add fixes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -12,7 +12,7 @@ function createGallery(items) {
             `<li><a class="gallery__item" href=${original}>
   <img class="gallery__image" src=${preview} alt="${description}" />
 </a></li>`,
-    )-=[[[[[[[[[[[]]]]]]]]]]]
+    )
         .join('');
 }
 
